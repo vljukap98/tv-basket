9833f2ed7f5825572a12509ebe14aa8bb6b646de
Luka Jaković <30662155+vljukap98@users.noreply.github.com>
2021-07-13 16:24:58 +0200

    Testing Tailwind CSS

--- code/src/Home.js
+++ code/src/Home.js
@@ -2,7 +2,6 @@ import MovieList from './movieList';
 import useFetch from './useFetch';
 
 const Home = () => {
-
     const { data: movies, isPending, error } = useFetch('http://localhost:3000/movies');
 
 
@@ -11,7 +10,7 @@ const Home = () => {
             { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
             { movies && <MovieList movies={ movies } title = "All movies"/> }
-            <p>This is home page, later it will show movies in card list views</p>
+            <p className="">This is home page, later it will show movies in card list views</p>
         </div> 
      );
 }
--- code/src/Navbar.js
+++ code/src/Navbar.js
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return ( 
-        <nav className="navbar">
-            <Link to="/"><img src={logo} alt="TvBasketLogo"/></Link>
-            <div className="links">
+        <nav className="flex space-x-10 justify-evenly">
+            <div>
+                <Link to="/"><img src={logo} alt="TvBasketLogo"/></Link>
+            </div>
+            <div className="space-x-36">
                 <Link to="/">HOME</Link>
                 <Link to="/sign_in">SIGN IN</Link>
                 <Link to="/register">REGISTER</Link>
                 <Link to="/about">ABOUT</Link>
+            </div>
+            <div className="flex justify-evenly space-x-2">
+                <p>Jon Doe</p>
+                <div className="border-4 w-2 h-2 border-indigo-500"></div>
             </div>
         </nav>
      );
